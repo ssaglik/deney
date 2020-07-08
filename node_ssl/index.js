322d5782c4efd7c07ea5d6aa2e0588324355ecd4
Serdar Sağlık <serdar.saglik@gmail.com>
2020-07-09 00:04:08 +0300

    deney

--- node_ssl/index.js
+++ node_ssl/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
+var https = require('https');
+var http = require('http');
 var app = express();
+var fs = require('fs');
+
+
+var options = {
+    key: fs.readFileSync('deney-key.pem'),
+    cert: fs.readFileSync('deney-cert.pem')
+}
+
 var path = require('path');
 
 // viewed at http://localhost:8080
